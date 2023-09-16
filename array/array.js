30a56dd8c8dcc3c0b1e67db966d04749afc0c9a6
mahmud <mahmudsoftware@gmail.com>
2023-09-16 08:34:42 -0700

    first simple array add

---
+++ array/array.js
@@ -0,0 +1,5 @@
+let students = ['Ronaldo','Messi','Salah','Bell','Karim Benzema']
+
+console.log(students[1]) //Show Messi 
+console.log(students[0]) //Show Ronaldo
+console.log(students[3]) //Show Bell
